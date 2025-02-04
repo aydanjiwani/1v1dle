d58b0f1a444a93d2e487101d7ca85b3d61e0b990
Aydan Jiwani <a9jiwani@uwaterloo.ca>
2025-02-03 23:14:09 -0500

    backend fix for deploy

--- backend/backend.go
+++ backend/backend.go
@@ -245,7 +245,11 @@ func loadValidGuesses(filename string) map[string]bool {
 }
 
 func enableCORS(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	origin := os.Getenv("CORS_ORIGIN")
+	if origin == "" {
+		origin = "http://localhost:5173" // Default for local development
+	}
+	w.Header().Set("Access-Control-Allow-Origin", origin)
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
